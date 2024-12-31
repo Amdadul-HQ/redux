641040983a15a388a2f29fbcff692d4455506203
Amdadul-HQ <rimonamdadul301@gmail.com>
2024-12-31 19:19:28 +0600

    add: counter with payload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
 import { decrement, increment } from './Redux/features/counter/counterSlice';
 
 function App() {
 
   const dispatch = useDispatch();
+  const {count} = useSelector((state) => state.counter);
   
   return (
     <section>
       <h1>Counter with Redux</h1>
       <div>
-        <button onClick={()=>dispatch(increment())}>
+        <button onClick={()=>dispatch(increment(5))}>
           Increment
         </button>
         <div>
-        0          
+        {count}      
         </div>
         <button onClick={()=> dispatch(decrement())}>
           Decrement
